d826235d9e459bccbb6caa62f49379dc38e9a556
Showichiro <65388586+Showichiro@users.noreply.github.com>
2024-04-22 18:41:23 +0000

    Deploying to gh-pages from @ Showichiro/tanstack-router-example@f33cf98c2fb77faaa1c299517f665b1497818ca4 🚀

---
+++ assets/index.lazy-D04ZV-wi.js
@@ -0,0 +1,1 @@
+import{g as t,h as o,j as e}from"./index-D2CyKILe.js";const a=t("/")({component:n});function n(){const s=o();return e.jsxs(e.Fragment,{children:[e.jsx("h1",{className:"kuma-3691895832",children:"Home"}),e.jsxs("p",{children:["Hello, ",s.user,"!"]})]})}export{a as Route};
--- assets/index.lazy-Txo4goCo.js
+++
@@ -1,1 +0,0 @@
-import{g as t,h as o,j as e}from"./index-Cz96cHFR.js";const a=t("/")({component:n});function n(){const s=o();return e.jsxs(e.Fragment,{children:[e.jsx("h1",{className:"kuma-3691895832",children:"Home"}),e.jsxs("p",{children:["Hello, ",s.user,"!"]})]})}export{a as Route};
